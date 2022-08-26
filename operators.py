73cb5d5ba2549d203a29a1ff4d00bd3f84b63615
SheetalBorkar98 <71390524+SheetalBorkar98@users.noreply.github.com>
2022-08-27 00:19:12 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,45 @@
+a = 55
+b = 4
+
+# ARITHMETIC OPERATIONS
+print(a+b)
+print("Addition = ",a+b)
+print("Subtraction = ",a-b)
+print("Multiplication = ",a*b)
+print("Division = ",a/b)
+print("Power = ",a**b)
+print("Floor division = ",a//b)
+print("Addition = ",a+b,'\n')
+
+# ASSIGNMENT OPERATORS
+a = 56
+a += 4
+print(a)
+
+a -= 10             # Updates value of a is used here.
+print(a)
+
+# COMPARISON OPERATOR
+print(a<b)
+print(b>a)
+print(a == b)
+print(a != b)
+print(a>=b)
+# Returns boolean values
+
+# LOGICAL OPERATORS
+print(b and a)
+print(a and b)
+        # both have different values
+print(b or a)
+print(a or b)
+        # both have different values
+print(not b)
+
+bool1 = False
+bool2 = True
+print(bool2 and bool1)
+print(bool2 or bool1)
+print(not bool1)
+
+
