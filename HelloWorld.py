20a81d6922909779d46aac6af0801a236c4d5857
SheetalBorkar98 <71390524+SheetalBorkar98@users.noreply.github.com>
2022-08-26 23:11:20 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,8 +1,16 @@
-# first basic program
+# first basic program (Single line comment using #)
 
 print("Hello World !")
-print('''    This is your
-    First Program''')
+# This will print whatever is written in single line inside print.
 
+print('''This is your
+First Program''')
+# The above syntax will print multiple lines(using triple quotes ''' ''')
+
+#Below is the multi line comment
 '''This is the most basic
 First program'''
+
+# ''' ''' inside and outside print makes a lot of difference.
+# Inside print it becomes a string literal.
+# Outside print it becomes a multi line comment.
